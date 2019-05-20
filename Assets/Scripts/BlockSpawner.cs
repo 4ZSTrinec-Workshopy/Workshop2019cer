63ae748ddf41c056829e7cd53fb6bc03409c3d8c
Adam077x <adan.heczko@gmail.com>
2019-05-20 18:23:46 +0200

    update

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ if - podmínka napr. pokud x se rovná 5 tak udělej...
+ void - funckce
+ class - třída s metodama a proměnýma
+ public - veřejná funckce, třida nebo proměná. Dá se k ní přistoupit z jiných tříd.
+ using - import sady nějakých funkcí...
+ Update - název funkce od Unity. Cyklus který se opakuje až do vypnutí programu.
+ Start - název funkce od Unity. Při vytvoření daného objektu ve scéně.
+ OnCollisionEnter - název funkce od Unity. Pří dotyku s objektem.
+*/
+
 public class BlockSpawner : MonoBehaviour
 {
     public GameObject obstacle; // Kostka
