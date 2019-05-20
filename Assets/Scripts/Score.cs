6c91bb86357085ce685e9c4e4b75fbb82e147703
Adam077x <adan.heczko@gmail.com>
2019-05-20 20:22:10 +0200

    Score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    int score = 0;
+    Text scoreText;
+
+    void Start()
+    {
+        InvokeRepeating("IncrementScore", 0, 1);
+        scoreText = this.GetComponent<Text>();
+    }
+
+    void IncrementScore() {
+        score++;
+        scoreText.text = "Score: " + score;
+    }
+}
